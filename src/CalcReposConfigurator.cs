db82974e0e253a5e6a74c31e60e131ce0b9d1461
Christian <cjaeckel@pol-it.de>
2018-11-21 13:38:59 +0100

    removed CalcNgn dependency from Tlabs.Data

---
+++ src/CalcReposConfigurator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+using Tlabs.Config;
+using Tlabs.Data.Processing;
+
+namespace Tlabs.CalcNgn {
+
+  ///<summary>Configures all data repositories as services.</summary>
+  public class CalcReposConfigurator : IConfigurator<IServiceCollection> {
+    ///<summary>Default time zone</summary>
+    public const string DEFAULT_TIME_ZONE= "W. Europe Standard Time";   //TODO: this is probably windows only
+
+    private ILogger log= App.Logger<CalcReposConfigurator>();
+
+    ///<inherit/>
+    public void AddTo(IServiceCollection services, IConfiguration cfg) {
+      services.TryAddSingleton<CalcNgn.Calculator>();
+      services.TryAddSingleton<CalcNgn.Intern.ICalcNgnModelParser, CalcNgn.Sgear.CalcNgnModelParser>();
+      services.AddScoped(typeof(DocCalcProcessorRepo));
+      log.LogDebug("Calc. repository services added.");
+    }
+
+    
+  }
+}
---
+++ src/Processing/DocSchemaCalcProcessor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Logging;
+
+using Tlabs.CalcNgn;
+using Tlabs.Data.Entity;
+using Tlabs.Data.Serialize;
+
+namespace Tlabs.Data.Processing {
+
+  /// <summary><see cref="DocSchemaProcessor"/> to perform <see cref="Calculator.Model"/> specific document computation(s).</summary>
+  public class DocSchemaCalcProcessor : DocSchemaProcessor {
+
+    private Calculator.Model calcNgnModel;
+
+    /// <summary>Ctor from <paramref name="schema"/>, <paramref name="docClassFactory"/>, <paramref name="docSeri"/> and <paramref name="calcNgn"/>.</summary>
+    public DocSchemaCalcProcessor(DocumentSchema schema, IDocumentClassFactory docClassFactory, IDynamicSerializer docSeri, Calculator calcNgn)
+    : base(schema, docClassFactory, docSeri) {
+      if (null == calcNgn) throw new ArgumentNullException(nameof(calcNgn));
+
+      if (schema.HasCalcModel) {    // check calc model
+        this.calcNgnModel= calcNgn.LoadModel(schema.CalcModelStream);
+        var impCnt= calcNgnModel.Definition.Imports.Count;
+        var expCnt= calcNgnModel.Definition.Exports.Count;
+        DocSchemaProcessor.Log.LogDebug("{schema} has {imp} import(s) and {exp} export(s).", schema.TypeId, impCnt, expCnt);
+        if (0 == impCnt + expCnt) DocSchemaProcessor.Log.LogWarning("No data import/export definition found in calcModel of {schema}.", schema.TypeId);
+      }
+    }
+
+    ///<summary>Perform any calc. model specific computation(s).</summary>
+    protected override object processBodyObject(object bodyObj, Func<object, IDictionary<string, object>> setupData) {
+      if (null == calcNgnModel) return bodyObj;
+      var model=   null != setupData
+                 ? setupData(bodyObj)
+                 : bodyAccessor.ToDictionary(bodyObj);
+#if DEBUG
+      SaveModel(Path.Combine(Path.GetDirectoryName(App.MainEntryPath), "calcNgnModel", schema.TypeName + "0.xls"));
+#endif
+      calcNgnModel.Compute(model);
+#if DEBUG
+      SaveModel(Path.Combine(Path.GetDirectoryName(App.MainEntryPath), "calcNgnModel", schema.TypeName + ".xls"));
+#endif
+      return bodyObj;
+    }
+
+    private void SaveModel(string path) {
+      if (null == calcNgnModel) return;
+      new DirectoryInfo(Path.GetDirectoryName(path)).Create();  //ensure path
+      using (var strm = File.Create(path)) {
+        calcNgnModel.Definition.WriteStream(strm);
+      }
+    }
+
+
+  }
+
+}
