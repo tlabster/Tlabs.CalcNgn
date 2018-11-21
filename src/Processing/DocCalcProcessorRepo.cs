5ad7fb2ea0ff7a16a27e530e3f74a805d8492bc3
Christian <cjaeckel@pol-it.de>
2018-11-21 13:38:59 +0100

    removed CalcNgn dependency from Tlabs.Data

---
+++ src/Processing/DocCalcProcessorRepo.cs
@@ -0,0 +1,27 @@
+﻿
+using Tlabs.Misc;
+using Tlabs.Data.Entity;
+
+namespace Tlabs.Data.Processing {
+
+  ///<summary>Repository of <see cref="DocSchemaCalcProcessor"/>.</summary>
+  public class DocCalcProcessorRepo : Intern.AbstractDocProcRepo<DocSchemaCalcProcessor> {
+    private Tlabs.CalcNgn.Calculator calcNgn;
+    private static readonly BasicCache<string, DocSchemaCalcProcessor> schemaCache= new BasicCache<string, DocSchemaCalcProcessor>();
+
+    ///<summary>Ctor from services.</summary>
+    public DocCalcProcessorRepo(Repo.IDocSchemaRepo schemaRepo,
+                            IDocumentClassFactory docClassFactory,
+                            Serialize.IDynamicSerializer docSeri,
+                            Tlabs.CalcNgn.Calculator calcNgn)
+    : base(schemaRepo, docClassFactory, docSeri)
+    {
+      this.calcNgn= calcNgn;
+    }
+
+    ///<inherit/>
+    protected override DocSchemaCalcProcessor createProcessor(DocumentSchema schema) => new DocSchemaCalcProcessor(schema, docClassFactory, docSeri, calcNgn);
+  }
+
+
+}
